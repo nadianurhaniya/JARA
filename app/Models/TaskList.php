18b3b5b1bb6001e174e5307a5028a97f82067ada
Muhammad Hafidh Al-Ghifari <alghifarihafidh@gmail.com>
2026-09-11 09:42:48 +0700

    feat(tugas): tambah migrasi dan model task list, tugas, subtugas

---
+++ app/Models/TaskList.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TaskListFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['name', 'description'])]
+class TaskList extends Model
+{
+    /** @use HasFactory<TaskListFactory> */
+    use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+}
