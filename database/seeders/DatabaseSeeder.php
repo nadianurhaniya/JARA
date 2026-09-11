48d9b49830004be44182fae777f05e4b40b4d0e9
Muhammad Hafidh Al-Ghifari <alghifarihafidh@gmail.com>
2026-09-11 09:42:52 +0700

    feat(tugas): tambah factory dan seeder task list, tugas, subtugas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Subtask;
+use App\Models\Task;
+use App\Models\TaskList;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +20,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        if (app()->environment('local')) {
+            TaskList::factory()
+                ->count(3)
+                ->for($user, 'owner')
+                ->has(
+                    Task::factory()
+                        ->count(5)
+                        ->for($user, 'owner')
+                        ->has(Subtask::factory()->count(2), 'subtasks')
+                )
+                ->create();
+        }
     }
 }
