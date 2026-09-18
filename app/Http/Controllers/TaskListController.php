9d1c62dcb57687c465f67cafb6dd03ad359a5a51
Muhammad Hafidh Al-Ghifari <alghifarihafidh@gmail.com>
2026-09-18 09:24:05 +0700

    feature(otorisasi): jaga create dan index task list lewat policy

--- app/Http/Controllers/TaskListController.php
+++ app/Http/Controllers/TaskListController.php
@@ -17,6 +17,8 @@ class TaskListController extends Controller
      */
     public function index(Request $request): View
     {
+        Gate::authorize('viewAny', TaskList::class);
+
         $taskLists = $request->user()
             ->taskLists()
             ->withCount('tasks')
@@ -31,6 +33,8 @@ class TaskListController extends Controller
      */
     public function create(): View
     {
+        Gate::authorize('create', TaskList::class);
+
         return view('task-lists.create');
     }
 
